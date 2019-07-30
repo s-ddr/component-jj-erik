527c6182e45f0f2a3008f48d2583b0cc2c1ea7fd
Erik Grubbs <erikgrubbs@gmail.com>
2019-07-30 14:45:40 -0700

    [prog] Cleans up cluster implementation

--- server/index.js
+++ server/index.js
@@ -21,27 +21,9 @@ app.use(cors());
 app.use('/api', router);
 
 if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-
-  // Fork workers.
   for (let i = 0; i < numCpus; i++) {
     cluster.fork();
   }
-
-  //Check if work id is died
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
-
 } else {
-  // This is Workers can share any TCP connection
-  // It will be initialized using express
-  console.log(`Worker ${process.pid} started`);
-
-  app.get('/cluster', (req, res) => {
-    let worker = cluster.worker.id;
-    res.send(`Running on worker with id ==> ${worker}`);
-  });
-
   app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 }
