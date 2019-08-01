2319d47db972e1224b2cdb77ae5181e57262bc1b
Erik Grubbs <erikgrubbs@gmail.com>
2019-08-01 12:23:31 -0700

    [init] inital commit to deploy
    Tried to trim all the fat, just experimenting!

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/carouselJson',  { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/carousel',  { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
--- stresstest/my-functions.js
+++
@@ -1,7 +0,0 @@
-const randomId = (userContext, events, done) => {
-  userContext.vars.id = (Math.floor(Math.random() * 10000000)) + 1;
-  return done();
-}
-
-
-module.exports = {randomId}
